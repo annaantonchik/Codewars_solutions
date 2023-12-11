ce330814f3635456ab19849f80cbb68b0b211704
Anna Antonchik <anyaaantonchic@gmail.com>
2023-12-11 16:29:48 +0100

    41 solution added

--- Codewars_solutions.py
+++ Codewars_solutions.py
@@ -1,5 +1,10 @@
-
-
+'''№41 String to list of integers. <7 kyu>
+Given a string containing a list of integers separated by commas, write the function string_to_int_list(s) that takes said string and returns a new list containing all integers present in the string, preserving the order.
+For example, give the string "-1,2,3,4,5", the function string_to_int_list() should return [-1,2,3,4,5]
+Please note that there can be one or more consecutive commas whithout numbers, like so: "-1,-2,,,,,,3,4,5,,6"
+'''
+def string_to_int_list(s):
+    return [int(i) for i in s.split(',') if i != '']
 
 '''№40 Integer depth <7 kyu>
 Write a function that can return the smallest value of an array or the index of that value. The function's 2nd parameter will tell whether it should return the value or the index.
