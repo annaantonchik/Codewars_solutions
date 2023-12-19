1f841fbf8c1e7a22aeb680ef2164817f452ed650
Anna Antonchik <anyaaantonchic@gmail.com>
2023-12-19 21:46:47 +0100

    46 solution added

--- Codewars_solutions.py
+++ Codewars_solutions.py
@@ -1,3 +1,19 @@
+'''№46 Square Matrix Multiplication <5 kyu>
+Write a function that accepts two square (NxN) matrices (two dimensional arrays), and returns the product of the two. Only square matrices will be given.
+How to multiply two square matrices:
+We are given two matrices, A and B, of size 2x2 (note: tests are not limited to 2x2). 
+Matrix C, the solution, will be equal to the product of A and B. To fill in cell [0][0] of matrix C, you need to compute: A[0][0] * B[0][0] + A[0][1] * B[1][0].
+More general: To fill in cell [n][m] of matrix C, you need to first multiply the elements in the nth row of matrix A by the elements in the mth column of matrix B, then take the sum of all those products. 
+This will give you the value for cell [m][n] in matrix C.
+'''
+def matrix_mult(a, b):
+    res = [[0] * len(a[0]) for _ in range(len(a))]
+    for i in range(len(b)):
+        for j in range(len(a)):
+            for k in range(len(a[0])):
+                res[i][j] += a[i][k] * b[k][j]
+    return res
+
 '''№45 Who likes it? <6 kyu>
 You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
 Implement the function which takes an array containing the names of people that like an item. 
